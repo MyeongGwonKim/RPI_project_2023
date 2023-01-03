838d392963f82b240dd13a8f2ccdaa462ace1f0b
MyeongKwonKim <ridsky13@gmail.com>
2023-01-03 16:26:52 +0900

    for_example.2023.01.03

---
+++ python/for_exam.py
@@ -0,0 +1,20 @@
+#숫자 0-9 출력
+
+asd = [9,8,7,6,4,4,1]
+
+# for i in asd:
+#     print(i)
+
+# ch='string'
+# for i in ch:
+#     print(i)
+
+
+# for i in range(7):
+#     if (i == asd[i]):
+#         print('일치')
+#     else:
+#         print("불일치")
+
+asd.sort()
+print(asd)
