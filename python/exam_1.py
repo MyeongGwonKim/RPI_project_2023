a2b0f040bd67885e407256f41c61e58dc0320e1d
MyeongKwonKim <ridsky13@gmail.com>
2023-01-03 16:31:46 +0900

    fixed_1

--- python/exam_1.py
+++ python/exam_1.py
@@ -1,9 +1,8 @@
 #정수 엔이 주어질 떄, 엔 이하의 짝수를 ㅁ두 더한 값을 리턴하도록 솔루션 함수를 작성해주셍.
 
 num=input('n값을 입력하세요.')
-a=int(num)
 sum=0
-for i in range(int(num)):
+for i in range(int(num)+1):
     if(i%2==0):
         sum = i + sum
 
